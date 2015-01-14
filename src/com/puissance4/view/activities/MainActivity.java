c23ccb20ea7ea724eb8c0b5a2195c00984c990a3
IA-64 <cyrille.fiore@gmail.com>
2015-01-14 01:43:08 +0100

    Lancement de partie en réseau débuggé.

--- src/com/puissance4/view/activities/MainActivity.java
+++ src/com/puissance4/view/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.puissance4.view.activities;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -10,10 +11,12 @@ import android.os.IBinder;
 import android.widget.Button;
 
 import com.example.Puissance4.R;
+import com.puissance4.configuration.GameConfiguration;
 import com.puissance4.controller.button_controllers.FriendListButtonListener;
 import com.puissance4.controller.button_controllers.FriendPlayerButtonListener;
 import com.puissance4.controller.button_controllers.NearPlayerButtonListener;
 import com.puissance4.controller.button_controllers.SettingsButtonListener;
+import com.puissance4.model.Party;
 import com.puissance4.server_com.network_handlers.NetworkComm;
 import com.puissance4.server_com.ping_service.PingService;
 
@@ -64,6 +67,37 @@ public class MainActivity extends Activity {
         friendPlayerButton.setOnClickListener(new FriendPlayerButtonListener(this));
         friendListButton.setOnClickListener(new FriendListButtonListener(this));
         settingsButton.setOnClickListener(new SettingsButtonListener(this));
+
+        if(getIntent().hasExtra("ADVNAME")) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancel(5);
+
+            Intent gameIntent = new Intent(this, GameActivity.class);
+
+            String[] players = new String[2];
+            players[0] = GameConfiguration.USERNAME;
+            players[1] = getIntent().getStringExtra("ADVNAME");
+
+            GameConfiguration.GRID_HEIGHT = getIntent().getIntExtra("X", 0);
+            GameConfiguration.GRID_WIDTH = getIntent().getIntExtra("Y", 0);
+
+            switch (NetworkComm.getInstance().answerProposal(true)) {
+                case 0:
+                    //CREATE PARTY WITH SELF AS FIRST PLAYER
+                    gameIntent.putExtra("party", new Party(players, GameConfiguration.GRID_HEIGHT, GameConfiguration.GRID_WIDTH));
+                    startActivity(gameIntent);
+                    break;
+                case 1:
+                    //CREATE PARTY WITH SELF AS SECOND PLAYER
+                    gameIntent.putExtra("party", new Party(players, GameConfiguration.GRID_HEIGHT, GameConfiguration.GRID_WIDTH));
+                    startActivity(gameIntent);
+                    break;
+                default:
+                    //ERREUR
+                    finish();
+                    break;
+            }
+        }
     }
 
     @Override
