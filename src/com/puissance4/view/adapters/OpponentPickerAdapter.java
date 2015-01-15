768e7d0ce4fa1cbff2ec776d93f12e372d5be15f
IA-64 <cyrille.fiore@gmail.com>
2015-01-15 23:03:32 +0100

    Ajout de l'affichage de la distance des autres joueurs dans les listes.

--- src/com/puissance4/view/adapters/OpponentPickerAdapter.java
+++ src/com/puissance4/view/adapters/OpponentPickerAdapter.java
@@ -2,6 +2,9 @@ package com.puissance4.view.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,9 +22,16 @@ import java.util.ArrayList;
  * Created by fred on 10/01/15.
  */
 public class OpponentPickerAdapter extends ArrayNetworkPlayerAdapter {
+    private Location myLocation;
 
     public OpponentPickerAdapter(Activity context, int resource, ArrayList<NetworkPlayer> objects) {
         super(context, resource, objects);
+        LocationManager locationManager = ((LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE));
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_MEDIUM);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, true));
     }
 
     @Override
@@ -31,17 +41,38 @@ public class OpponentPickerAdapter extends ArrayNetworkPlayerAdapter {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.friend_item, null);
         }
+
         final NetworkPlayer opponent = objects.get(position);
+
+        String opponentDistanceText = null;
+        if (myLocation != null) {
+            float[] results = new float[3];
+            Location.distanceBetween(myLocation.getLatitude(), myLocation.getLongitude(), opponent.getLatitude(), opponent.getLongitude(), results);
+            float opponentDistance = results[0];
+            if (opponentDistance < 1000) {
+                opponentDistanceText = Integer.toString(Math.round(opponentDistance)) + "m";
+            } else {
+                opponentDistanceText = Integer.toString(Math.round(opponentDistance / 1000)) + "km";
+            }
+        }
+
         if (opponent != null) {
             ImageView statusView = (ImageView) v.findViewById(R.id.imageFriendItem);
             TextView friendText = (TextView) v.findViewById(R.id.textFriendItem);
+            TextView distanceText = (TextView) v.findViewById(R.id.textFriendDistanceItem);
             Button playButton = (Button) v.findViewById(R.id.buttonFriendItem);
             switch (opponent.getStatus()) {
                 case 0:
+                    if (opponentDistanceText != null) {
+                        distanceText.setText(opponentDistanceText);
+                    }
                     statusView.setImageResource(R.drawable.icon_green_dot);
                     playButton.setText(R.string.playButton);
                     break;
                 case 1:
+                    if (opponentDistanceText != null) {
+                        distanceText.setText(opponentDistanceText);
+                    }
                     statusView.setImageResource(R.drawable.icon_orange_dot);
                     playButton.setVisibility(View.GONE);
                     break;
